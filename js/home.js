f7f06f28b69402c2edec16960261e4462b4b2125
Mrgeegee <34643085+Mrgeegee@users.noreply.github.com>
2020-07-30 23:28:57 +0200

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+var vid = document.getElementById("video");
+function removeControls(video)
+ {
+	 video.removeAttribute('controls');
+ }
+	window.onload=removeControls(video);
+
+
+
